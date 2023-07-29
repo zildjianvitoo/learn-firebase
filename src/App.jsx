918dcd9784df897ae7eb33070426bbc5a2a198f3
ZildjianVito <zildjianvito07@gmail.com>
2023-07-29 23:31:58 +0700

    first initialize project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>hahahah</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/config/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAJpmNHSmyMUrzI7PEiL9V5Aogw34ySYiE",
+  authDomain: "learn-firebase-4b0e3.firebaseapp.com",
+  projectId: "learn-firebase-4b0e3",
+  storageBucket: "learn-firebase-4b0e3.appspot.com",
+  messagingSenderId: "973326495890",
+  appId: "1:973326495890:web:a142f19441082ffdda364b",
+  measurementId: "G-8E2MHMKDMD",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
